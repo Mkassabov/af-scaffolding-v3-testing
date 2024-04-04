a96487fa4bda5e7e289a85348a4f7b9dd7242de7
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2024-04-04 19:36:45 -0400

    changes

--- src/scaffold.ts
+++ src/scaffold.ts
@@ -1,6 +1,23 @@
-import { child } from "~/child";
+import type { SmartGithubTree } from "@aerofoil/core/soft/lazyGitTree/Github";
+import type { ServerCore } from "@aerofoil/logger";
+// import { child } from "~/child";
 
-export async function main({ logger, selfGitTree, projectGitTree }) {
-	logger.debug("Hello from scaffold.ts");
-	logger.debug(child());
+export async function main({
+	logger,
+	selfGitTree,
+	projectGitTree,
+}: {
+	logger: ServerCore;
+	selfGitTree: SmartGithubTree;
+	projectGitTree: SmartGithubTree;
+}) {
+	const assetTree = await selfGitTree.getEntry("/assets");
+	if (!assetTree.success || assetTree.value.type !== "tree") {
+		logger.error("No asset folder found");
+		throw new Error("No asset folder found");
+	}
+	logger.info("Adding assets folder");
+	await projectGitTree.addTree(await assetTree.value.retriveFullTree());
+	await projectGitTree.commit("Add assets folder");
+	logger.success("Added assets folder");
 }
