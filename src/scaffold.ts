34e4208a364cc090ba8acd4c3ec76185a7a5f41c
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2024-04-04 23:48:48 -0400

    Update scaffold.ts

--- src/scaffold.ts
+++ src/scaffold.ts
@@ -12,15 +12,22 @@ export async function main({
 	selfGitTree: SmartGithubTree;
 	projectGitTree: SmartGithubTree;
 }) {
+	logger.debug(0);
 	const assetTree = await selfGitTree.getEntry("/assets");
+	logger.debug(1);
 	if (assetTree.type !== "tree") {
 		logger.error("No asset folder found");
 		throw new Error("No asset folder found");
 	}
+	logger.debug(2);
 	logger.info("Adding assets folder");
 	await projectGitTree.deleteFile("test.md").catch(() => {});
+	logger.debug(3);
 	await projectGitTree.addTree("/assets", await assetTree.retriveFullTree());
+	logger.debug(4);
 	logger.debug(await projectGitTree.retriveFullTree());
+	logger.debug(5);
 	await projectGitTree.commit("Add assets folder");
+	logger.debug(6);
 	logger.success("fixed assets folder");
 }
