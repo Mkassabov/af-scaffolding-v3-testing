2fb70c2db2fb2f19d46c97e883263f6899fddf26
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2024-04-04 23:37:55 -0400

    Update scaffold.ts

--- src/scaffold.ts
+++ src/scaffold.ts
@@ -1,5 +1,5 @@
 import type { SmartGithubTree } from "@aerofoil/core/soft/lazyGitTree/Github";
-import { throwFail } from "@aerofoil/core/soft/lazyGitTree/utils";
+// import { throwFail } from "@aerofoil/core/soft/lazyGitTree/utils";
 import type { ServerCore } from "@aerofoil/logger";
 // import { child } from "~/child";
 
@@ -12,14 +12,17 @@ export async function main({
 	selfGitTree: SmartGithubTree;
 	projectGitTree: SmartGithubTree;
 }) {
-	const assetTree = await selfGitTree.getEntry("/assets").then(throwFail);
-	if (assetTree.type !== "tree") {
+	const assetTree = await selfGitTree.getEntry("/assets");
+	if (!assetTree.success || assetTree.value.type !== "tree") {
 		logger.error("No asset folder found");
 		throw new Error("No asset folder found");
 	}
 	logger.info("Adding assets folder");
 	await projectGitTree.deleteFile("test.md");
-	await projectGitTree.addTree("/assets", await assetTree.retriveFullTree());
+	await projectGitTree.addTree(
+		"/assets",
+		await assetTree.value.retriveFullTree(),
+	);
 	await projectGitTree.commit("Add assets folder");
 	logger.success("fixed assets folder");
 }
